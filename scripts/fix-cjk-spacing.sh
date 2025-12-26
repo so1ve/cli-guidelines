da80341724ed7d42bf1d588bceee00de98efc745
onevcat <onevcat@gmail.com>
2025-12-26 23:02:21 +0900

    Fix CJK spacing after inline tags

--- scripts/fix-cjk-spacing.sh
+++ scripts/fix-cjk-spacing.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-# Post-process HTML to remove newlines/spaces after Chinese punctuation
+# Post-process HTML to remove spaces/newlines after Chinese punctuation
 # Usage: Run after `hugo` build
 
-find public -name "*.html" -exec perl -i -p0e 's/([，。、；：""''（）！？》])\n+/\1/g' {} \;
+find public -name "*.html" -exec perl -i -p0e '
+  s/([，。、；：""''（）！？》])\n+/\1/g;
+  s/([，。、；：""''（）！？》])((?:<\/(?:strong|b|em|i|span|a|code|kbd|sup|sub)>)+)\s+((?:<(?:strong|b|em|i|span|a|code|kbd|sup|sub)[^>]*>)*)([\x{4E00}-\x{9FFF}])/\1\2\3\4/g;
+  s/([，。、；：""''（）！？》])\s+((?:<(?:strong|b|em|i|span|a|code|kbd|sup|sub)[^>]*>)*)([\x{4E00}-\x{9FFF}])/\1\2\3/g;
+' {} \;
 
 echo "CJK spacing fixed in public/"
