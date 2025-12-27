2877fef9da700c192b0b7865d7607ef02ac6c3c6
onevcat <onevcat@gmail.com>
2025-12-27 10:11:53 +0900

    Remove spaces after CJK period

--- scripts/fix-cjk-spacing.sh
+++ scripts/fix-cjk-spacing.sh
@@ -4,6 +4,7 @@
 
 find public -name "*.html" -exec perl -CSDA -i -p0e '
   use utf8;
+  s/。\s+/。/g;
   s/([，。、；：""''（）！？》])\n+/\1/g;
   s/([，。、；：""''（）！？》])((?:<\/(?:strong|b|em|i|span|a|code|kbd|sup|sub)>)+)\s+((?:<(?:strong|b|em|i|span|a|code|kbd|sup|sub)[^>]*>)*)([\x{4E00}-\x{9FFF}])/\1\2\3\4/g;
   s/([，。、；：""''（）！？》])\s+((?:<(?:strong|b|em|i|span|a|code|kbd|sup|sub)[^>]*>)*)([\x{4E00}-\x{9FFF}])/\1\2\3/g;
